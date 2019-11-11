c9aef22baade225e393981d44268467ac69bd0a0
Chris Baptiste <chrisbaptiste83@gmail.com>
2019-11-11 04:07:34 -0700

    update Application controller, to use a set session generated by secret session gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views' 
-    enable :sessions
-		set :session_secret, "password_security"
-
-
+    set :sessions, true
+		set :session_secret, ENV['SESSION_SECRET']
+  
   get "/" do
     erb :welcome
   end 
@@ -30,6 +29,6 @@ class ApplicationController < Sinatra::Base
   
     end
 
-  end
+  end 
 
 end 
