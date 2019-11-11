32d9c1fad9bde4571f63a4312a4752e5107a5d15
Chris Baptiste <chrisbaptiste83@gmail.com>
2019-11-10 23:13:15 -0700

    update user model to verify password uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base 
     has_secure_password
-    validates_presence_of :username, :email, :password
-    validates :username, :uniqueness => {:case_sensitive => false}
-     
+    validates :username, presence: true, uniqueness: true
     has_many :cocktail_recipes
 end
