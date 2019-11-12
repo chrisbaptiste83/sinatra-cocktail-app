a09a651f9e2f122d9aaf2d194e160a31de749ba6
Chris Baptiste <chrisbaptiste83@gmail.com>
2019-11-12 07:58:31 -0700

    update patch route, include all params keys when updating recipe

--- app/controllers/cocktail_recipes_controller.rb
+++ app/controllers/cocktail_recipes_controller.rb
@@ -1,5 +1,5 @@
 class CocktailRecipesController < ApplicationController
-  # GET: /cocktail_recipes
+  
   get "/cocktail_recipes" do
     if logged_in?
       @user = current_user 
@@ -11,7 +11,7 @@ class CocktailRecipesController < ApplicationController
 
   end
 
-  # GET: /cocktail_recipes/new
+  
   get "/cocktail_recipes/new" do 
     if logged_in?
       erb :"/cocktail_recipes/new.html"  
@@ -20,28 +20,28 @@ class CocktailRecipesController < ApplicationController
     end
   end
 
-  # POST: /cocktail_recipes
+  
     post "/cocktail_recipes" do 
-    if params[:cocktail_name] == "" 
-      flash[:message] = "Cocktail name cannot be blank. Please enter a cocktail name and complete all fields."
-      erb :"cocktail_recipes/new.html"
-    else
-     @cocktail_recipe = CocktailRecipe.create(params)
-     @cocktail_recipe.user = current_user 
-     @cocktail_recipe.save 
-     flash[:message] = "Your cocktail has been created."
-     redirect "/cocktail_recipes/#{@cocktail_recipe.id}"
+      if params[:cocktail_name].empty? || params[:ingredients].empty? || params[:instructions].empty?
+        flash[:message] = "Please complete all fields in order to upload a recipe."
+        erb :"cocktail_recipes/new.html"
+      else
+        @cocktail_recipe = CocktailRecipe.create(cocktail_name: params[:cocktail_name], ingredients: params[:ingredients], instructions: params[:instructions])
+        @cocktail_recipe.user = current_user 
+        @cocktail_recipe.save 
+        flash[:message] = "Your cocktail has been created."
+        redirect "/cocktail_recipes/#{@cocktail_recipe.id}"
       end
     end
 
 
-  # GET: /cocktail_recipes/5
+ 
   get "/cocktail_recipes/:id" do 
     @cocktail_recipe = CocktailRecipe.find(params[:id]) 
     erb :"/cocktail_recipes/show.html"
   end
    
-  # GET: /cocktail_recipes/5/edit
+ 
   get "/cocktail_recipes/:id/edit" do 
     @cocktail_recipe = CocktailRecipe.find(params[:id])
             
@@ -51,21 +51,22 @@ class CocktailRecipesController < ApplicationController
           flash[:message] = "You must be the cocktail recipe's owner in order to edit the recipe."
           redirect "/cocktail_recipes"
         end
-      end
+  end
 
-  # PATCH: /cocktail_recipes/5
+  
   patch "/cocktail_recipes/:id" do 
-    @cocktail_recipe = current_user.cocktail_recipes.find(params[:id])
-        if params[:cocktail_recipe] != ""
-          @cocktail_recipe.update(params[:cocktail_recipe]) 
+    @cocktail_recipe = CocktailRecipe.find(params[:id])
+        if params[:cocktail_name].empty? 
+          flash[:message] = "Updated cocktail recipe must have a name."
+          redirect "/cocktail_recipes/#{@cocktail_recipe.id}/edit" 
+        else
+          @cocktail_recipe.update(cocktail_name: params[:cocktail_name], ingredients: params[:ingredients], instructions: params[:instructions]) 
           flash[:message] = "Your recipe has been updated."
           redirect "/cocktail_recipes"
-        else
-          redirect "/cocktail_recipes/#{@cocktail_recipe.id}/edit"
         end
      end
 
-  # DELETE: /cocktail_recipes/5/delete
+  
   delete "/cocktail_recipes/:id/delete" do 
     @cocktail_recipe = CocktailRecipe.find(params[:id])
           if @cocktail_recipe.user_id == current_user.id
@@ -75,6 +76,6 @@ class CocktailRecipesController < ApplicationController
           else
             redirect "/cocktail_recipes"
          end
-        end
+   end
    
 end
