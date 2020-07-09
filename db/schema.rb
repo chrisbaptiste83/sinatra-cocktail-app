0ddbf01ec83a99af7c25c8a28314ff3723637fa9
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-07-09 02:59:43 -0700

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20191104152723) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cocktail_recipes", force: :cascade do |t|
     t.string   "cocktail_name"
     t.string   "image_url"
