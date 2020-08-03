edc87d8130dcbb4f8c378dd89d9140c044b08aec
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-08-03 09:43:59 -0700

    add slug method and find by slug methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :username, presence: true, uniqueness: true
     has_many :cocktail_recipes 
-    has_many :comments
+    has_many :comments 
+
+    def slug
+        self.username
+      end
+    
+    def self.find_by_slug(slug) 
+        User.all.find{|user| user.slug == slug}
+    end
 end
