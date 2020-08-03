6b415c22661b78b4b0055607c493a6091960cbdb
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-08-03 09:42:30 -0700

    add updated date method to cocktail recipe model

--- app/models/cocktail_recipe.rb
+++ app/models/cocktail_recipe.rb
@@ -1,5 +1,11 @@
 class CocktailRecipe < ActiveRecord::Base 
     belongs_to :user 
     validates :cocktail_name, :ingredients, :instructions, presence: true 
-    has_many :comments
+    has_many :comments 
+
+ 
+   def created_at_date 
+    self.created_at.strftime("%a %b %e %Y at %T") 
+   end   
+
 end
